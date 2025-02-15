88a33872f08c0bd5fc4e4d365ffc028e303c88b8
yam-k <yam.k.o212@gmail.com>
2025-02-15 23:55:09 +0900

    セットアップスクリプトの追加とそれに合わせたファイルの整理

---
+++ utils/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# ホームディレクトリに設定ファイルのシンボリックリンクを貼る。
+
+# "|"の前が、sources/からの設定ファイルへの相対パス。
+# "|"の後が、作成するシンボリックリンクの$HOMEからの相対パス。
+sources=(
+    "emacs.d|.emacs.d"
+    "Xresources|.Xresources"
+    "xinitrc|.xinitrc"
+)
+sources_dir=$(cd $(dirname $0)/../sources; pwd)
+
+for source in ${sources[@]}; do
+    source_file=${sources_dir}/${source%|*}
+    target_file=${HOME}/${source#*|}
+
+    # 作成するシンボリックリンクと同じ名前のファイルが有る時に、
+    # 古いファイルを削除するか確認する。
+    if [ -L ${target_file} ] || [ -f ${target_file} ]; then
+        echo "${target_file} already exists. Delete old setting? (yes/no)"
+        read answer
+
+        if [ -z ${answer} ] || [ ${answer} != "yes" ]; then
+            continue
+        fi
+    elif [ -d ${target_file} ]; then
+        echo "${target_file} is directory. Delete this and make symlink? (yes/no)"
+        read answer
+
+        if [ -z ${answer} ] || [ ${answer} != "yes" ]; then
+            continue
+        else
+            rm -rf ${target_file}
+        fi
+    fi
+
+    # --forceオプション付きでシンボリックリンクを作成。
+    echo "Making symbolic link ${target_file} to ${source_file}"
+    ln -s -f ${source_file} ${target_file}
+    echo "Done."
+done
